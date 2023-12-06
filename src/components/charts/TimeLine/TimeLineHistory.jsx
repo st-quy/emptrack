09bd2a9b8375561c7b8965febb7f6e1a5f1935bf
Duy <duy@devplus.edu.vn>
2023-12-06 17:00:33 +0700

    fix build

--- src/components/charts/TimeLine/TimeLineHistory.jsx
+++ src/components/charts/TimeLine/TimeLineHistory.jsx
@@ -1,26 +1,28 @@
 import React from 'react';
 import { Timeline } from 'antd';
-const TimeLineHistory = () => (
-  <Timeline
-    reverse
-    mode="left"
-    items={[
-      {
-        label: '2015-09-01',
-        children: 'Create a services',
-      },
-      {
-        label: '2015-09-01 09:12:11',
-        children: 'Solve initial network problems',
-      },
-      {
-        children: 'Technical testing',
-      },
-      {
-        label: '2015-09-01 09:12:11',
-        children: 'Network problems being solved',
-      },
-    ]}
-  />
-);
+const TimeLineHistory = () => {
+  return (
+    <Timeline
+      reverse
+      mode="left"
+      items={[
+        {
+          label: '2015-09-01',
+          children: 'Create a services',
+        },
+        {
+          label: '2015-09-01 09:12:11',
+          children: 'Solve initial network problems',
+        },
+        {
+          children: 'Technical testing',
+        },
+        {
+          label: '2015-09-01 09:12:11',
+          children: 'Network problems being solved',
+        },
+      ]}
+    />
+  );
+};
 export default TimeLineHistory;
