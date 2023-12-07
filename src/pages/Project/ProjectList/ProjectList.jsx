c354ebe3ce81145ddbbbd13f7628df845535b785
DuyDevPlus <148930323+DuyDevPlus@users.noreply.github.com>
2023-12-07 16:36:47 +0700

    Feat/et-16 (#12)
    * duy

--- src/pages/Project/ProjectList/ProjectList.jsx
+++ src/pages/Project/ProjectList/ProjectList.jsx
@@ -1,5 +1,5 @@
 import { DeleteOutlined, EyeOutlined } from '@ant-design/icons';
-import { Card, Input, Pagination, Space, Table, Tooltip } from 'antd';
+import { Card, Input, Pagination, Space, Table, Tooltip, Modal,message  } from 'antd';
 import { debounce } from 'lodash';
 import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
@@ -10,58 +10,82 @@ import Breadcrumb from '../../../components/molecules/Breadcrumb/Breadcrumb';
 import { axiosInstance } from '../../../config/axios';
 
 const ProjectList = () => {
-  const navigate = useNavigate();
-  const { t } = useTranslation();
   const [data, setData] = useState([]);
+  const { t } = useTranslation();
   // const [searchedText, setSearchedText] = useState('');
+  const [deletedProjectId] = useState(null);
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [selectedProjectId, setSelectedProjectId] = useState(null);
+  const navigate = useNavigate();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageSize, setPageSize] = useState(10);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        await axiosInstance.get('projects').then((response) => {
-          setData(response.data);
-        });
+        const result = await axiosInstance.get('projects').then((response) =>  response.data);
+        const filterDeleted = result.filter(item => !item.deletedAt)
+        setData(filterDeleted);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
     };
     fetchData();
-  }, []);
+  }, [deletedProjectId]);
+
+  const handleDelete = (projectId) => {
+    setSelectedProjectId(projectId);
+    // setCurrentPage(1); // Đặt lại trang hiện tại về 1 khi xóa dự án
+    setShowDeleteModal(true);
+  };
+  const handleConfirmDelete = async () => {
+    try {
+      await axiosInstance.delete(`projects/${selectedProjectId}`).then(() => {
+
+        message.success('Dự án đã được xóa thành công!');
+        // Loại bỏ dự án đã bị xóa khỏi mảng data
+        setData(data.filter((item) => item.id !== selectedProjectId));
+
+        setSelectedProjectId(null);
+        setShowDeleteModal(false);
+      });
+    } catch (error) {
+      console.error('Error deleting project:', error);
+    }
+  };
+
+  const handleCancelDelete = () => {
+    setSelectedProjectId(null);
+    setShowDeleteModal(false);
+  };
+
 
   const columns = [
     {
       title: t('TABLE.ACTIONS'),
       key: 'action',
-      width: 60,
-      fixed: 'left',
+      width: 100,
       render: (text, record) => (
         <span>
           <Tooltip title="Delete">
-            <Button
-              type="link"
-              icon={<DeleteOutlined />}
-              onClick={() => handleDelete(record.key)}
-            />
+            <Button type="link" icon={<DeleteOutlined />} onClick={() => handleDelete(record.id)} />
           </Tooltip>
           <Tooltip title="View">
-            <Button
-              type="link"
-              icon={<EyeOutlined />}
-              onClick={() => handleView(record.key)}
-            />
+            <Button type="link" icon={<EyeOutlined />} onClick={() => handleView(record.id)} />
           </Tooltip>
         </span>
       ),
     },
 
+
     {
       title: t('TABLE.MANAGER'),
       dataIndex: 'manager',
       key: 'manager',
-      width: 80, // Giảm kích thước của cột "Manager"
-      sorter: {
-        compare: (a, b) => a.chinese - b.chinese,
-        multiple: 3,
+      width: 80,
+      sorter: (a, b) => a.manager[0].name.localeCompare(b.manager[0].name),
+      ellipsis: {
+        showTitle: false,
       },
       render: (managers) => (
         <span>
@@ -75,11 +99,9 @@ const ProjectList = () => {
       title: t('BREADCRUMB.PROJECTS'),
       dataIndex: 'name',
       key: 'name',
-      width: 80, // Giảm kích thước của cột "Projects"
-      sorter: {
-        compare: (a, b) => a.chinese - b.chinese,
-        multiple: 3,
-      },
+      render: (text) => <a>{text}</a>,
+      width: 150,
+      sorter: (a, b) => a.name.localeCompare(b.name),
     },
     // {
     //   title: t('TABLE.MEMBERS'),
@@ -130,9 +152,9 @@ const ProjectList = () => {
       title: t('TABLE.START DATE'),
       dataIndex: 'startDate',
       key: 'startDate',
-      width: 80, // Giảm kích thước của cột "Start Date"
+      width: 90,
       sorter: {
-        compare: (a, b) => a.chinese - b.chinese,
+        compare: (a, b) => new Date(a.startDate) - new Date(b.startDate),
         multiple: 3,
       },
       ellipsis: {
@@ -148,9 +170,9 @@ const ProjectList = () => {
       title: t('TABLE.END DATE'),
       dataIndex: 'endDate',
       key: 'endDate',
-      width: 80, // Giảm kích thước của cột "End Date"
+      width: 90,
       sorter: {
-        compare: (a, b) => a.chinese - b.chinese,
+        compare: (a, b) => new Date(a.endDate) - new Date(b.endDate),
         multiple: 3,
       },
       ellipsis: {
@@ -166,11 +188,7 @@ const ProjectList = () => {
       title: t('STATUS.STATUS'),
       dataIndex: 'status',
       key: 'status',
-      width: 60, // Giảm kích thước của cột "Status"
-      sorter: {
-        compare: (a, b) => a.status.localeCompare(b.status),
-        multiple: 3,
-      },
+      width: 90,
       ellipsis: {
         showTitle: false,
       },
@@ -189,6 +207,11 @@ const ProjectList = () => {
           </span>
         </Tooltip>
       ),
+      filters: [
+        { text: 'Active', value: 'active' },
+        { text: 'Inactive', value: 'inactive' },
+      ],
+      onFilter: (value, record) => record.status === value,
     },
   ];
 
@@ -212,41 +235,59 @@ const ProjectList = () => {
           style={{ marginBottom: 8, width: 300, marginTop: 8 }}
           onChange={(e) => setSearchedText(e.target.value)}
         />
-        <Table
+         <Table
           columns={columns}
-          dataSource={data.filter(
-            (item) =>
-              // item.id.toString().includes(searchedText.toLowerCase()) ||
-              (item.manager &&
-                item.manager.some((manager) =>
-                  manager.name
-                    .toLowerCase()
-                    .includes(searchedText.toLowerCase()),
-                )) ||
-              // (item.member &&
-              //   item.member.some(
-              //     (member) =>
-              //       member.name.toLowerCase().includes(searchedText.toLowerCase()) ||
-              //       member.role.toLowerCase().includes(searchedText.toLowerCase())
-              //   )) ||
-              // item.description.toLowerCase().includes(searchedText.toLowerCase()) ||
-              item.startDate
-                .toLowerCase()
-                .includes(searchedText.toLowerCase()) ||
-              item.endDate.toLowerCase().includes(searchedText.toLowerCase()) ||
-              item.status.toLowerCase().includes(searchedText.toLowerCase()) ||
-              item.name.toLowerCase().includes(searchedText.toLowerCase()),
-          )}
-          scroll={{ y: 'calc(100vh - 400px)' }}
+          dataSource={data
+            .filter(
+              (item) =>
+                // !item.deletedAt &&
+                // Chỉ hiển thị các dự án chưa bị xóa
+                (item.manager &&
+                  item.manager.some(
+                    (manager) => manager.name.toLowerCase().includes(searchedText.toLowerCase())
+                  )) ||
+                (item.member &&
+                  item.member.some(
+                    (member) =>
+                      member.name.toLowerCase().includes(searchedText.toLowerCase()) ||
+                      member.role.toLowerCase().includes(searchedText.toLowerCase())
+                  )) ||
+                item.technical.toLowerCase().includes(searchedText.toLowerCase()) ||
+                item.description.toLowerCase().includes(searchedText.toLowerCase()) ||
+                item.startDate.toLowerCase().includes(searchedText.toLowerCase()) ||
+                item.endDate.toLowerCase().includes(searchedText.toLowerCase()) ||
+                item.status.toLowerCase().includes(searchedText.toLowerCase()) ||
+                item.name.toLowerCase().includes(searchedText.toLowerCase())
+            )
+            .slice((currentPage - 1) * pageSize, currentPage * pageSize)}
+          scroll={{
+            x: 1500,
+            y: 'calc(100vh - 400px)',
+          }}
           pagination={false}
         />
         <Pagination
-          total={25}
+          total={data.filter((item) => !item.deletedAt).length}
+          current={currentPage}
+          pageSize={pageSize}
           showSizeChanger
-          showTotal={(total) => t('TABLE.TOTAL', { total })}
-          style={{ marginTop: '5px' }}
+          showTotal={(total) => `Total ${total} items`}
+          style={{ marginTop: '25px' }}
+          onChange={(page, pageSize) => {
+            setCurrentPage(page);
+            setPageSize(pageSize);
+          }}
         />
+
       </Card>
+      <Modal
+        title="Confirm Delete"
+        visible={showDeleteModal}
+        onOk={handleConfirmDelete}
+        onCancel={handleCancelDelete}
+      >
+        <p>Are you sure you want to delete this project?</p>
+      </Modal>
     </div>
   );
 };
