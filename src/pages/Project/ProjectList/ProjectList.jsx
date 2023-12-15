11d5eb4bfbb222c21a88b6fb1eb389cc8d7233e7
ngoc-tong <ngoc.tong-ojt07@devplus.edu.vn>
2023-12-15 09:06:16 +0700

    fix vn-eng

--- src/pages/Project/ProjectList/ProjectList.jsx
+++ src/pages/Project/ProjectList/ProjectList.jsx
@@ -90,7 +90,7 @@ const ProjectList = () => {
     current: currentPage,
     pageSize: pageSize,
     showSizeChanger: true,
-    showTotal: (total) => t('TABLE.TOTAL_EMPLOYEES', { total }),
+    showTotal: (total) => t('TABLE.TOTAL', { total }),
     className: 'my-3',
     onChange: (page, pageSize) => {
       setCurrentPage(page);
@@ -212,7 +212,6 @@ const ProjectList = () => {
       render: (id, record, index) => {
         ++index;
         return (currentPage - 1) * pageSize + index;
-        ;
       },
     },
     {
